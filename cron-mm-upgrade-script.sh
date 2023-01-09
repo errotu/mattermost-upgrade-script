3226afcaef95cef21466d5b264d6651d0b7283bd
Keith Carichner Jr <keith@saintsofvirtue.org>
2023-01-08 20:55:33 -0500

    Script cleanup and variable fix.

--- cron-mm-upgrade-script.sh
+++ cron-mm-upgrade-script.sh
@@ -20,25 +20,25 @@ function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4
 
 # Define upgrade function
 function upgrade () {
-    if curl -I "https://releases.mattermost.com/$MMVERSION/mattermost-$MMVERSION-linux-amd64.tar.gz" 2>&1 | grep -q -w "200\|301" ; then
+    if curl -I "https://releases.mattermost.com/$latestVersion/mattermost-$latestVersion-linux-amd64.tar.gz" 2>&1 | grep -q -w "200\|301" ; then
         echo -e "CURL: Response OKAY! Continuing upgrade...\n"
     else
         echo -e "CURL: URL REPORTS DOWN!!! Please check Mattermost version URL and try again. Exiting upgrade..." && exit 1
     fi
-    wget https://releases.mattermost.com/$MMVERSION/mattermost-$MMVERSION-linux-amd64.tar.gz
-    tar -xf mattermost-$MMVERSION-linux-amd64.tar.gz --transform='s,^[^/]\+,\0-upgrade,'
-    rm mattermost-$MMVERSION-linux-amd64.tar.gz
-    sudo systemctl stop mattermost
-    cd /opt/ && sudo cp -ra mattermost/ mattermost-back-$(date +'%F-%H-%M')/
-    sudo find mattermost/ mattermost/client/ -mindepth 1 -maxdepth 1 \! \( -type d \( -path mattermost/client -o -path mattermost/client/plugins -o -path mattermost/config -o -path mattermost/logs -o -path mattermost/plugins -o -path mattermost/data \) -prune \) | sort | sudo xargs rm -r
-    sudo mv mattermost/plugins/ mattermost/plugins~ && sudo mv mattermost/client/plugins/ mattermost/client/plugins~
-    sudo chown -hR mattermost:mattermost ~/mattermost-upgrade/
-    sudo cp -an ~/mattermost-upgrade/. mattermost/
-    sudo rm -r ~/mattermost-upgrade/
+    wget https://releases.mattermost.com/$latestVersion/mattermost-$latestVersion-linux-amd64.tar.gz -P /tmp/
+    cd /tmp/ && tar -xf mattermost-$latestVersion-linux-amd64.tar.gz --transform='s,^[^/]\+,\0-upgrade,'
+    rm /tmp/mattermost-$latestVersion-linux-amd64.tar.gz
+    systemctl stop mattermost
+    cp -ra /opt/mattermost/ /opt/mattermost-back-$(date +'%F-%H-%M')/
+    find /opt/mattermost/ mattermost/client/ -mindepth 1 -maxdepth 1 \! \( -type d \( -path mattermost/client -o -path mattermost/client/plugins -o -path mattermost/config -o -path mattermost/logs -o -path mattermost/plugins -o -path mattermost/data \) -prune \) | sort | sudo xargs rm -r
+    mv /opt/mattermost/plugins/ /opt/mattermost/plugins~ && mv /opt/mattermost/client/plugins/ /opt/mattermost/client/plugins~
+    chown -hR mattermost:mattermost /tmp/mattermost-upgrade/
+    cp -an /tmp/mattermost-upgrade/. /opt/mattermost/
+    rm -r /tmp/mattermost-upgrade/
     # Active CAP_NET_BIND_SERVICE to allow Mattermost to bind to low ports - uncomment the below commands if the Mattermost instance is serving web requests
-    #cd /opt/mattermost && sudo setcap cap_net_bind_service=+ep ./bin/mattermost
-    cd /opt/mattermost && sudo rsync -au plugins~/ plugins && sudo rm -rf plugins~ && sudo rsync -au client/plugins~/ client/plugins && sudo rm -rf client/plugins~
-    sudo systemctl start mattermost
+    #cd /opt/mattermost && setcap cap_net_bind_service=+ep ./bin/mattermost
+    cd /opt/mattermost && rsync -au plugins~/ plugins && rm -rf plugins~ && rsync -au client/plugins~/ client/plugins && rm -rf client/plugins~
+    systemctl start mattermost
     echo -e "Upgrade complete!"
 }
 
