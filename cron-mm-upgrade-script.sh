cc0e1ee9ec043b2d6030fdc064f34f1f81834dd9
errotu <errotu@users.noreply.github.com>
2023-02-17 00:17:58 +0100

    Add database backup

--- cron-mm-upgrade-script.sh
+++ cron-mm-upgrade-script.sh
@@ -2,7 +2,7 @@
 
 ################################################################################################
 # The following script updates the local Mattermost instance to the newest version via cron.   #
-# A backup of the instance will be taken.                        	                             #
+# A backup of the instance will be taken.                                                      #
 ################################################################################################
 
 # Establsh deployed Mattermost version via mmctl
@@ -16,8 +16,31 @@ echo -e "Date/Time:" $(date)
 echo -e "Deployed Mattermost version: $deployedVersion"
 echo -e "Latest Mattermost version: $latestVersion"
 
+# Establish date/time variable
+date=$(date +'%F-%H-%M')
+
+# Establish database variables from Mattermost config.json
+DATABASE=$(cat ~/mattermost/config/config.json | jq --raw-output '.SqlSettings.DriverName')
+DB_USER=$(cat ~/mattermost/config/config.json | jq --raw-output '.SqlSettings.DataSource' | cut -d: -f2 | tr -d '/')
+DB_PASS=$(cat ~/mattermost/config/config.json | jq --raw-output '.SqlSettings.DataSource' | cut -d: -f3 | cut -d@ -f1)
+
 # Define version function
 function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
+
+# Define database backup function
+function dbbackup () {
+    if [[ $DATABASE == "mysql" ]]; then
+        echo -e "Database is MySQL. Conducting database backup..."
+        mysqldump -u $DB_USER -p $DB_PASS mattermost > /opt/mattermost-back-$date/database-backup-$date.sql
+        echo -e "Database backup complete."
+    elif [[ $DATABASE == "postgres" ]]; then
+        echo -e "Database is PostgreSQL. Conducting database backup..."
+        pg_dump -U $DB_USER mattermost > /opt/mattermost-back-$date/database-backup-$date.sql
+        echo -e "Database backup complete."
+    else
+        echo -e "Unable to determine database type. A backup will not be conducted."
+    fi
+}
 
 # Define upgrade function
 function upgrade () {
@@ -30,6 +53,7 @@ function upgrade () {
     cd ~/ && tar -xf mattermost-$latestVersion-linux-amd64.tar.gz --transform='s,^[^/]\+,\0-upgrade,'
     rm ~/mattermost-$latestVersion-linux-amd64.tar.gz
     supervisorctl stop mattermost
+    dbbackup
     cp -ra ~/mattermost/ ~/mattermost-back-$(date +'%F-%H-%M')/
     find ~/mattermost/ ~/mattermost/client/ -mindepth 1 -maxdepth 1 \! \( -type d \( -path ~/mattermost/client -o -path ~/mattermost/client/plugins -o -path ~/mattermost/config -o -path ~/mattermost/logs -o -path ~/mattermost/plugins -o -path ~/mattermost/data \) -prune \) | sort | xargs rm -r
     mv ~/mattermost/plugins/ ~/mattermost/plugins~ && mv ~/mattermost/client/plugins/ ~/mattermost/client/plugins~
